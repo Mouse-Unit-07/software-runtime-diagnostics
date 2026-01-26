2c9cc4f365a01def2db745815fb306649a739292
Ryuichi Lin <ryuno107lin@gmail.com>
2026-01-25 22:53:47 -0500

    feat: add code to pass telemetry log initialization

--- runtime_diagnostics/runtime_diagnostics.h
+++ runtime_diagnostics/runtime_diagnostics.h
@@ -10,10 +10,20 @@
 /*----------------------------------------------------------------------------*/
 /*                             Public Definitions                             */
 /*----------------------------------------------------------------------------*/
-/* none */
+struct log_entry {
+    const char *file;
+    uint32_t timestamp;
+    uint16_t line;
+    uint16_t runtime_diagnostic_identifier;
+};
+
+enum {LOG_CAPACITY = 16};
 
 /*----------------------------------------------------------------------------*/
 /*                         Public Function Prototypes                         */
 /*----------------------------------------------------------------------------*/
+void init_runtime_diagnostics();
+void deinit_runtime_diagnostics();
+struct log_entry *get_telemetry_log(void);
 
 #endif /* RUNTIME_DIAGNOSTICS_H_ */
--- runtime_diagnostics/tests/test_runtime_diagnostics.cpp
+++ runtime_diagnostics/tests/test_runtime_diagnostics.cpp
@@ -6,35 +6,37 @@
 /*============================================================================*/
 
 /* scratch notes- a list of tests:
-- do all 3 buffers initialize to 0's?
+- does the telemetry log initialize to 0?
+- do all 3 logs initialize to 0's?
 - does the logging function append the right data to a buffer?
 - does the logging function continue to append the right data when called continuously?
 - is the right data stored to each buffer when called w/ different run levels?
 - is the right data stored to each buffer when continuously called w/ different run levels?
 - does calling each macro add a new entry to the buffer?
-  - RUNTIME_TELEMETRY
-  - RUNTIME_WARNING
-  - RUNTIME_ERROR
+    - RUNTIME_TELEMETRY
+    - RUNTIME_WARNING
+    - RUNTIME_ERROR
 - does calling RUNTIME_ERROR assert the error flag?
 - is the error callback function not called when it's not set yet?
 - does calling RUNTIME_ERROR call the error callback function when it's set?
 - can you keep pushing to the buffer until capacity is reached?
 - is the right behavior observed in response to reaching capacity for:
-  - warning- assert the error flag and call the error callback
+    - warning- assert the error flag and call the error callback
 - does pushing past the buffer capacity exhibit the right behavior?:
-  - telemetry- overwrite from the oldest element
-  - warning- overwrite from the oldest element, and assert the error flag and call the error callback
-  - error- overwrite from the newest element
+    - telemetry- overwrite from the oldest element
+    - warning- overwrite from the oldest element, and assert the error flag and call the error callback
+    - error- overwrite from the newest element
 */
 
 /*============================================================================*/
 /*                               Include Files                                */
 /*============================================================================*/
-
 extern "C" {
-
+  #include <stdint.h>
+  #include "runtime_diagnostics.h"
 }
 
+#include <cstdint>
 #include <CppUTest/TestHarness.h>
 
 /*============================================================================*/
@@ -52,19 +54,20 @@ TEST_GROUP(RuntimeDiagnosticsTest)
 {
     void setup() override
     {
-        
+        init_runtime_diagnostics();
     }
 
     void teardown() override
     {
-        
+        deinit_runtime_diagnostics();
     }
 };
 
 /*============================================================================*/
 /*                                    Tests                                   */
 /*============================================================================*/
-TEST(RuntimeDiagnosticsTest, InitializesBuffersToZero)
+TEST(RuntimeDiagnosticsTest, TelemetryLogIsInitializedToZero)
 {
-    FAIL("Start TDD");
+    uint8_t expected[sizeof(*get_telemetry_log())] = {0};
+    MEMCMP_EQUAL(expected, get_telemetry_log(), sizeof(*get_telemetry_log()));
 }
